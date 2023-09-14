bbc40830387fe24facf04edd5045402f396ca212
Marta Escribano <martaeg309@gmail.com>
2023-09-14 19:25:46 +0200

    first project added

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -13,14 +13,14 @@ function Intro() {
           <img className='profileimg' src="fotoMia.png" alt="#"></img>
         </div>
         <div>
-          <p className="div-width center">Soy desarrolladora Full-Stack junior con experiencia previa en relaciones con  clientes, así como en el ámbito de la comunicación y redes sociales. 
+          <p className="div-width justify">Soy desarrolladora Full-Stack junior con experiencia previa en relaciones con  clientes, así como en el ámbito de la comunicación y redes sociales. 
           En esta página, te presento los proyectos más importantes que he realizado hasta el momento, usando algunas tecnologías como html, CSS, JavaScript, Node.js, Angular o React. </p>
         </div>
         <div className="div-width">
           <h2>Me puedes contactar en... </h2>
           <div className="div-contact">
           <img src="linkedin.png" alt="logo_linkedin" className="logo"></img>
-          <a href= "https://www.linkedin.com/in/marta-escribano-garcia">Linkedin</a>
+          <a className="linkedin" href= "https://www.linkedin.com/in/marta-escribano-garcia"target="_blank" rel="noreferrer">Linkedin</a>
           </div>
           <div className="div-contact">
           <img src="mail.png" alt="logo_mail" className="logo"></img>
--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,46 +1,165 @@
-import React from 'react'
-import './Works.css'
+import React from "react";
+import "./Works.css";
 
 function Works() {
   return (
-    <div className='works-section'>
-    <h2>WORKS</h2>
-      <div className='prohjects-main-wrapper'>
-        <div className='project-wrapper'>
-            <img  className='project-img'src='https://cdn.dribbble.com/users/214929/screenshots/3813349/exploring_scrolling_animation.gif' alt="proyecto"></img>
-            <h3>Project 1</h3>
-            <p>Work description</p>
-            <a href='https://github.com/'>
-                <img className='github-icon' src='https://cdn-icons-png.flaticon.com/512/25/25231.png' alt="github"></img>
+    <div className="works-section">
+      <h2 className="title">CONOCE MIS PROYECTOS</h2>
+      <div className="works-cards">
+        <div className="card-one">
+          <div className="project-pokedex">
+            <h2>POKEAPI</h2>
+            <p>
+              Hazte con todos! Partiendo de la{" "}
+              <a
+                className="pokeapi"
+                href="https://pokeapi.co/api/v2/pokemon/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                POKEAPI
+              </a>
+              se crea una página web que pinta todos los pokemons de la misma,
+              así como algunas de sus propiedades. Se aplica además un buscador.{" "}
+            </p>
+            <a
+              href="https://martaesg.github.io/pokedex/"
+              target="_blank"
+              rel="noreferrer"
+              className="link-project"
+            >
+              ¡Vamos a verlo!{" "}
             </a>
+          </div>
+          <div className="works-descrip">
+            <div>
+              <h3>Tecnologías utilizadas</h3>
+              <img src="html.png" alt="html_logo" className="logos"></img>
+              <img src="css.png" alt="css_logo" className="logos"></img>
+              <img
+                src="JavaScript-logo.png"
+                alt="js_logo"
+                className="logos"
+              ></img>
+            </div>
+            <div>
+              <h3>Ver el código en GitHub</h3>
+              <a href="https://github.com/">
+                <img
+                  className="logos"
+                  src="https://cdn-icons-png.flaticon.com/256/25/25231.png"
+                  alt="github_pokedex"
+                ></img>
+              </a>
+            </div>
+          </div>
         </div>
-        <div className='project-wrapper'>
-            <img  className='project-img'src='https://cdn.dribbble.com/users/214929/screenshots/3813349/exploring_scrolling_animation.gif' alt="proyecto"></img>
-            <h3>Project 1</h3>
-            <p>Work description</p>
-            <a href='https://github.com/'>
-                <img className='github-icon' src='https://cdn-icons-png.flaticon.com/512/25/25231.png' alt="github"></img>
-            </a>
+        <div className="card-one">
+          <div className="projects-main-wrapper">
+            <div className="project-pokedex">
+              <h2>PokeApi</h2>
+              <p>
+                Hazte con todos! Partiendo de la{" "}
+                <a
+                  className="pokeapi"
+                  href="https://pokeapi.co/api/v2/pokemon/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  POKEAPI
+                </a>{" "}
+                se crea una página web que pinta todos los pokemons de la misma,
+                así como algunas de sus propiedades. Se aplica además un
+                buscador.{" "}
+              </p>
+              <a
+                href="https://martaesg.github.io/pokedex/"
+                target="_blank"
+                rel="noreferrer"
+                className="link-project"
+              >
+                ¡Vamos a verlo!{" "}
+              </a>
+            </div>
+          </div>
+          <div className="works-descrip">
+            <div>
+              <h3>Tecnologías utilizadas</h3>
+              <img src="html.png" alt="html_logo" className="logos"></img>
+              <img src="css.png" alt="css_logo" className="logos"></img>
+              <img
+                src="JavaScript-logo.png"
+                alt="js_logo"
+                className="logos"
+              ></img>
+            </div>
+            <div>
+              <h3>Ver el código en GitHub</h3>
+              <a href="https://github.com/">
+                <img
+                  className="logos"
+                  src="https://cdn-icons-png.flaticon.com/256/25/25231.png"
+                  alt="github_pokedex"
+                ></img>
+              </a>
+            </div>
+          </div>
         </div>
-        <div className='project-wrapper'>
-            <img  className='project-img'src='https://cdn.dribbble.com/users/214929/screenshots/3813349/exploring_scrolling_animation.gif' alt="proyecto"></img>
-            <h3>Project 1</h3>
-            <p>Work description</p>
-            <a href='https://github.com/'>
-                <img className='github-icon' src='https://cdn-icons-png.flaticon.com/512/25/25231.png' alt="github"></img>
-            </a>
-        </div>
-        <div className='project-wrapper'>
-            <img  className='project-img'src='https://cdn.dribbble.com/users/214929/screenshots/3813349/exploring_scrolling_animation.gif' alt="proyecto"></img>
-            <h3>Project 1</h3>
-            <p>Work description</p>
-            <a href='https://github.com/'>
-                <img className='github-icon' src='https://cdn-icons-png.flaticon.com/512/25/25231.png' alt="github"></img>
-            </a>
+        <div className="card-one">
+          <div className="projects-main-wrapper">
+            <div className="project-pokedex">
+              <h2>PokeApi</h2>
+              <p>
+                Hazte con todos! Partiendo de la{" "}
+                <a
+                  className="pokeapi"
+                  href="https://pokeapi.co/api/v2/pokemon/"
+                  target="_blank"
+                  rel="noreferrer"
+                  
+                >
+                  POKEAPI
+                </a>{" "}
+                se crea una página web que pinta todos los pokemons de la misma,
+                así como algunas de sus propiedades. Se aplica además un
+                buscador.{" "}
+              </p>
+              <a
+                href="https://martaesg.github.io/pokedex/"
+                target="_blank"
+                rel="noreferrer"
+                className="link-project"
+              >
+                ¡Vamos a verlo!{" "}
+              </a>
+            </div>
+          </div>
+          <div className="works-descrip">
+            <div>
+              <h3>Tecnologías utilizadas</h3>
+              <img src="html.png" alt="html_logo" className="logos"></img>
+              <img src="css.png" alt="css_logo" className="logos"></img>
+              <img
+                src="JavaScript-logo.png"
+                alt="js_logo"
+                className="logos"
+              ></img>
+            </div>
+            <div>
+              <h3>Ver el código en GitHub</h3>
+              <a href="https://github.com/">
+                <img
+                  className="logos"
+                  src="https://cdn-icons-png.flaticon.com/256/25/25231.png"
+                  alt="github_pokedex"
+                ></img>
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Works
+export default Works;
