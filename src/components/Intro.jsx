3fe536e7b95b07ade8d9328a48c1185672265080
Marta Escribano <martaeg309@gmail.com>
2023-09-14 16:44:45 +0200

    portfolio, header finished

---
+++ src/components/Intro.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import './Intro.css'
+
+function Intro() {
+  return (
+    <div>
+      <div className="intro-wrapper">
+        <div className="div-width center">
+          <h1>MARTA ESCRIBANO</h1>
+          <h2>Full-stack Developer</h2>
+        </div>
+        <div>
+          <img className='profileimg' src="fotoMia.png" alt="#"></img>
+        </div>
+        <div>
+          <p className="div-width center">Soy desarrolladora Full-Stack junior con experiencia previa en relaciones con  clientes, así como en el ámbito de la comunicación y redes sociales. 
+          En esta página, te presento los proyectos más importantes que he realizado hasta el momento, usando algunas tecnologías como html, CSS, JavaScript, Node.js, Angular o React. </p>
+        </div>
+        <div className="div-width">
+          <h2>Me puedes contactar en... </h2>
+          <div className="div-contact">
+          <img src="linkedin.png" alt="logo_linkedin" className="logo"></img>
+          <a href= "https://www.linkedin.com/in/marta-escribano-garcia">Linkedin</a>
+          </div>
+          <div className="div-contact">
+          <img src="mail.png" alt="logo_mail" className="logo"></img>
+          <p>marta.escribanog@hotmail.com</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Intro;
---
+++ src/components/Works.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import './Works.css'
+
+function Works() {
+  return (
+    <div className='works-section'>
+    <h2>WORKS</h2>
+      <div className='prohjects-main-wrapper'>
+        <div className='project-wrapper'>
+            <img  className='project-img'src='https://cdn.dribbble.com/users/214929/screenshots/3813349/exploring_scrolling_animation.gif' alt="proyecto"></img>
+            <h3>Project 1</h3>
+            <p>Work description</p>
+            <a href='https://github.com/'>
+                <img className='github-icon' src='https://cdn-icons-png.flaticon.com/512/25/25231.png' alt="github"></img>
+            </a>
+        </div>
+        <div className='project-wrapper'>
+            <img  className='project-img'src='https://cdn.dribbble.com/users/214929/screenshots/3813349/exploring_scrolling_animation.gif' alt="proyecto"></img>
+            <h3>Project 1</h3>
+            <p>Work description</p>
+            <a href='https://github.com/'>
+                <img className='github-icon' src='https://cdn-icons-png.flaticon.com/512/25/25231.png' alt="github"></img>
+            </a>
+        </div>
+        <div className='project-wrapper'>
+            <img  className='project-img'src='https://cdn.dribbble.com/users/214929/screenshots/3813349/exploring_scrolling_animation.gif' alt="proyecto"></img>
+            <h3>Project 1</h3>
+            <p>Work description</p>
+            <a href='https://github.com/'>
+                <img className='github-icon' src='https://cdn-icons-png.flaticon.com/512/25/25231.png' alt="github"></img>
+            </a>
+        </div>
+        <div className='project-wrapper'>
+            <img  className='project-img'src='https://cdn.dribbble.com/users/214929/screenshots/3813349/exploring_scrolling_animation.gif' alt="proyecto"></img>
+            <h3>Project 1</h3>
+            <p>Work description</p>
+            <a href='https://github.com/'>
+                <img className='github-icon' src='https://cdn-icons-png.flaticon.com/512/25/25231.png' alt="github"></img>
+            </a>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Works
